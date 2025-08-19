001d7621337af3035e41199ab4f68525df719f12
scherepi <48649270+scherepi@users.noreply.github.com>
2025-08-19 15:54:33 -0500

    Added extensive documentation. Also, test Nano heartbeats!

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,16 @@
 #![no_main] // disable all Rust-level entry points (don't look for a main function)
 
 #[cfg(test)]
+// Used to define our custom testing framework because we can't use Rust's standard one.
 pub fn test_runner(tests: &[&dyn Fn()]) {
-	println!("Running {} tests", tests.len());
+	println!("Running {} tests", tests.len()); // First, print out how many tests we're running.
 	for test in tests {
-		test();
+		test(); // Call each function we found.
 	}
 }
 
 
-mod vga_buffer;
+mod vga_buffer; // Include our module for interacting with the VGA buffer, a memory-mapped hardware abstraction of the terminal.
 
 use core::panic::PanicInfo;
 
@@ -20,14 +21,14 @@ use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
 	// The _info parameter contains info about the panic's cause.
-	println!("{}", _info);
-	loop {}
+	println!("{}", _info); // Right now, use the VGA buffer println macro to print panic info.
+	loop {} // Pause processes by creating an infinite loop.
 }
 
 #[unsafe(no_mangle)] // don't mangle the name of this function when compiling, literally name it _start
 pub extern "C" fn _start() -> ! {
 	// this function is the entry point, since the linker looks for a function named _start by default
-	println!("Test!!");
+	println!("Test!!"); // Call our println macro to write to the VGA buffer directly.
 	println!("It is so cool that {}", "this works");
 	
 	loop {}
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -3,10 +3,10 @@ use spin::Mutex;
 use core::fmt;
 use volatile::Volatile;
 
-#[allow(dead_code)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u8)]
-pub enum Color {
+#[allow(dead_code)] 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)] // Try to automatically ensure that our custom enum will have all of these traits: be copyable, comparable, and debuggable.
+#[repr(u8)] // Tells the compiler to represent this enum's values as an unsigned 8 bit integer.
+pub enum Color { // Basic enum of VGA buffer compatible colors.
 	Black = 0,
 	Blue = 1, 
 	Green = 2,
@@ -26,75 +26,78 @@ pub enum Color {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(transparent)]
-struct ColorCode(u8);
+#[repr(transparent)] // Instruct the compiler to inherit the size of its datatype (u8 in this case)
+struct ColorCode(u8); // Declare the ColorCode type
 
-impl ColorCode {
-	fn new(foreground: Color, background: Color) -> ColorCode {
+impl ColorCode { // Actually define the ColorCode's implementation
+	fn new(foreground: Color, background: Color) -> ColorCode { // Constructor for the ColorCode struct
 		ColorCode((background as u8) << 4 | (foreground as u8))
 	}
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(C)]
-struct ScreenChar {
-	ascii_character: u8,
-	color_code: ColorCode,
+#[repr(C)] // Tells the compiler to represent this the way C's compiler would (shortcut for compatibility)
+struct ScreenChar { // Defines how a character within the VGA buffer is defined:
+	ascii_character: u8, // An 8-bit unsigned int ASCII character value...
+	color_code: ColorCode, // ... and the color code for said character, being a background and foreground.
 }
 
-const BUFFER_HEIGHT: usize = 25;
-const BUFFER_WIDTH: usize = 80;
+const BUFFER_HEIGHT: usize = 25; // VGA buffer is 25 rows 
+const BUFFER_WIDTH: usize = 80;	// and 80 columns
 
 #[repr(transparent)]
 struct Buffer {
+	// Defines an abstraction for the VGA buffer. The Volatile wrapper type provides volatile memory access to the buffer directly.
 	chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
-pub struct Writer {
+pub struct Writer { // Defines a type with which to write to the VGA buffer, by providing info about its size and the memory address to which it's mapped.
 	column_position: usize,
 	color_code: ColorCode,
-	buffer: &'static mut Buffer,
+	buffer: &'static mut Buffer, // static, mutable pointer directly to the buffer in memory.
 }
 
-impl fmt::Write for Writer {
+impl fmt::Write for Writer { // Implement fmt::Write on the Writer for macro compatibility
 	fn write_str(&mut self, s: &str) -> fmt::Result {
-		self.write_string(s);
-		Ok(())
+		self.write_string(s); // Call this struct's usual write_string class
+		Ok(()) // Return an OK object, because an fmt::Result is expected.
 	}
 }
 
 impl Writer {
+	// implementation for the Writer struct.
 	pub fn write_byte(&mut self, byte: u8) {
-		match byte {
-			b'\n' => self.new_line(),
-			byte => {
+		match byte { // Switch statement for the u8 byte to write:
+			b'\n' => self.new_line(), // If it's just a newline char, call the newline func
+			byte => { // Otherwise...
 				if self.column_position >= BUFFER_WIDTH { 
-					self.new_line(); 
+					self.new_line(); // If we're at the end of a line, call newline 
 				}
 				
-				let row = BUFFER_HEIGHT - 1;
+				let row = BUFFER_HEIGHT - 1; // Push everything up and set the row to the bottom
 				let col = self.column_position;
 
 				let color_code = self.color_code;
-				self.buffer.chars[row][col].write(ScreenChar {
+				self.buffer.chars[row][col].write(ScreenChar { // Call the write function with a new ScreenChar object!
 					ascii_character: byte,
 					color_code,
 				});
-				self.column_position += 1;
+				self.column_position += 1; // Move the cursor one character to the right.
 			}
 		}
 	}
 
-	pub fn write_string(&mut self, s: &str) {
+	pub fn write_string(&mut self, s: &str) { // Basically just call the write function over and over.
 		for byte in s.bytes(){
 			match byte {
-				0x20..=0x7e | b'\n' => self.write_byte(byte),
-				_ => self.write_byte(0xfe),
+				0x20..=0x7e | b'\n' => self.write_byte(byte), // Valid byte
+				_ => self.write_byte(0xfe), // Invalid: Write a nothing byte.
 			}
 		}
 	}
 	
-	pub fn new_line(&mut self) {
+	pub fn new_line(&mut self) { // New line function
+		// Basically, move everything up a row and clear the bottom one for new characters.
 		for row in 1..BUFFER_HEIGHT {
 			for col in 0..BUFFER_WIDTH {
 				let character = self.buffer.chars[row][col].read();
@@ -106,6 +109,7 @@ impl Writer {
 	 }
 
 	 fn clear_row(&mut self, row: usize) {
+	 	// Write ' ' to every single character on a line.
 		let blank = ScreenChar {
 			ascii_character: b' ',
 			color_code: self.color_code,
@@ -116,17 +120,19 @@ impl Writer {
 	 }
 }
 
-use lazy_static::lazy_static;
+use lazy_static::lazy_static; // Necessary to make the compiler wait until everything else has been initialized
 
 lazy_static! {
+	// If we don't use lazy_static!, we run into an error because it attempts to initialize WRITER
+	// at the very beginning of the program.
 	pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
 		column_position: 0,
-		color_code: ColorCode::new(Color::Pink, Color::Black),
-		buffer: unsafe { &mut *(0xb8000 as *mut Buffer) }
+		color_code: ColorCode::new(Color::Pink, Color::Black), // Feel free to change!
+		buffer: unsafe { &mut *(0xb8000 as *mut Buffer) } // Use an unsafe block to access memory directly
 	});
 }
 
-
+// Define macros to make things easier: println! and print! become shortcuts to write directly to the buffer.
 #[macro_export]
 macro_rules! print {
 	($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
@@ -138,7 +144,7 @@ macro_rules! println {
 	($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
 }
 
-#[doc(hidden)]
+#[doc(hidden)] // Hide this from the auto-generated documentation.
 pub fn _print(args: fmt::Arguments) {
 	use core::fmt::Write;
 	WRITER.lock().write_fmt(args).unwrap();
