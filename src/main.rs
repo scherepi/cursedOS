d6c32b4b27eacdfb41a4d7e6c2b9b5e53dca47f5
scherepi <48649270+scherepi@users.noreply.github.com>
2025-08-19 14:40:13 -0500

    set up vga_buffer macros!

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-/// This function is called on panic
+// This function is called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
 	// The _info parameter contains info about the panic's cause.
@@ -15,9 +15,8 @@ fn panic(_info: &PanicInfo) -> ! {
 #[unsafe(no_mangle)] // don't mangle the name of this function when compiling, literally name it _start
 pub extern "C" fn _start() -> ! {
 	// this function is the entry point, since the linker looks for a function named _start by default
-	use core::fmt::Write;
-	vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-	write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+	println!("Test!!");
+	println!("It is so cool that {}", "this works");
 	
 	loop {}
 }
