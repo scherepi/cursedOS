077897b0818032708e0edd6c49cb9b8d4c15fe23
louisa :3 <louisakavindu@gmail.com>
2025-10-15 12:48:23 +0300

    better frame allocation + address translation bc im tweaking :P

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,17 @@ pub fn init() {
 	unsafe { interrupts::PICS.lock().initialize() };
 	x86_64::instructions::interrupts::enable();
 }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode { Success = 0x10, Failed = 0x11}
+pub fn exit_qemu(exit_code: QemuExitCode) -> ! {
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+    unreachable!()
+}
 
 pub fn hlt_loop() -> ! {
 	loop {
